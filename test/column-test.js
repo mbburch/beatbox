13ce38994c1fc44e9e9451a06d97826b25b111c7
russelleh <russell.west.eh@gmail.com>
2015-12-13 10:55:26 -0700

    Refactor tests

--- test/column-test.js
+++ test/column-test.js
@@ -6,23 +6,26 @@ const Board = require('../lib/board');
 const Column = require('../lib/column');
 const Note = require('../lib/note');
 
+let column;
+let note;
+before(function () {
+  note = new Note(Date.now() + 1000);
+  var note_2 = new Note(Date.now() + 2000);
+  var note_3 = new Note(Date.now() + 3000);
+  var notes = [note, note_2, note_3];
+  column = new Column(106, 90, notes);
+});
+
 describe('Column', function () {
   it('has an input button', function () {
-    var column = new Column(106);
     assert.equal(106, column.inputButton);
   });
 
   it('has a x varible', function () {
-    var column = new Column(106, 90);
     assert.equal(90, column.x);
   });
 
   it('can have notes', function () {
-    var note_1 = new Note(Date.now() + 1000);
-    var note_2 = new Note(Date.now() + 2000);
-    var note_3 = new Note(Date.now() + 3000);
-    var notes = [note_1, note_2, note_3];
-    var column = new Column(106, 90, notes);
-    assert.equal(note_1, column.notes[0])
+    assert.equal(note, column.notes[0])
   });
 });
